0f91212d83f82da0bd330e9fbe82549c2d308000
KMarchila21 <159220482+KMarchila21@users.noreply.github.com>
2024-02-17 12:59:53 -0600

    melissa
    prueba melissa

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main(int argc, char** argv) {
+	int a = 4; // declaramos e inicializamos una variable entera
+	int b = 1; // declaramos e inicializamos una variable entera
+	
+	int suma = a+b; // realizamos operacion 
+	// imprimimos en pantalla el resultado de la suma 
+	
+	cout<<"La suma es:"<<suma<<endl;
+
+	return 0;
+}
